f5116d00f9bd6aa6e127907382edb895d5158848
sarahlahdan-dot <sarah.lahdan@gmail.com>
2025-12-17 19:53:51 +0300

    little JS

---
+++ app.js
@@ -0,0 +1,33 @@
+let numbers = [];
+let score = 0;
+
+const numbersDisplay = document.getElementById('numbersDisplay');
+const startBtn = document.getElementById('startBtn');
+const checkBtn = document.getElementById('checkBtn');
+const result = document.getElementById('result');
+const scoreDiv = document.getElementById('score');
+const inputs = [
+  document.getElementById('input0'),
+  document.getElementById('input1'),
+  document.getElementById('input2')
+];
+
+
+//functions 
+
+function startGame() {
+  numbers = [];
+  numbersDisplay.innerHTML = '';
+  result.textContent = '';
+
+  inputs.forEach(input => {
+    input.value = '';
+    input.classList.remove('correct', 'incorrect');
+  });
+
+  // Generate 3 random numbers (1–50)
+  for (let i = 0; i < 3; i++) {
+    numbers.push(Math.floor(Math.random() * 50) + 1);
+  }}
+
+  
